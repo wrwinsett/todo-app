04a31e020dae2e6cabb0e51810d1de4dc497cd2b
william winsett <wrwinsett@gmail.com>
2023-09-19 15:59:16 -0400

    made tasks order by date

--- script.js
+++ script.js
@@ -2,34 +2,66 @@ const inputBox = document.getElementById("input-box");
 const timeInput = document.getElementById("time-input");
 const listContainer = document.getElementById("list-container");
 
-function addTask() {
-  const task = inputBox.value;
-  const time = timeInput.value;
+let tasksDated = [];
 
-  if (task === "") {
+function addTask() {
+  const taskText = inputBox.value;
+  const date = new Date(timeInput.value);
+
+  if (taskText === "") {
     alert("You must write something!");
     return;
   }
 
-  if (!time) {
+  if (!date || isNaN(date)) {
     alert("please pick a time and day!");
     return;
   } else {
-    const parsedTime = new Date(time);
-    const formatTime = parsedTime.toLocaleString();
+    const task = { text: taskText, date: date };
+    tasksDated.push(task);
 
-    let li = document.createElement("li");
-    li.innerHTML = task + " at " + formatTime + ".";
+    tasksDated.sort((a, b) => a.date - b.date);
 
-    listContainer.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
+    // const parsedTime = new Date(time);
+    // const formatTime = parsedTime.toLocaleString();
+
+    // let li = document.createElement("li");
+    // li.innerHTML = task + " at " + formatTime + ".";
+
+    // listContainer.appendChild(li);
+    // let span = document.createElement("span");
+    // span.innerHTML = "\u00d7";
+    // li.appendChild(span);
   }
+
+  displayTasks();
 
   inputBox.value = "";
   timeInput.value = "";
   saveData();
+}
+
+function displayTasks() {
+  // Clear the list container
+  listContainer.innerHTML = "";
+
+  // Iterate over the sorted tasks and create list items
+  tasksDated.forEach((task) => {
+    const li = document.createElement("li");
+    const formatTime = task.date.toLocaleString();
+    li.innerHTML = `${task.text} at ${formatTime}.`;
+    listContainer.appendChild(li);
+
+    const span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  });
+}
+
+function showTask() {
+  tasksDated.sort((a, b) => a.date - b.date);
+  displayTasks();
+  listContainer.innerHTML = localStorage.getItem("data");
 }
 
 listContainer.addEventListener(
